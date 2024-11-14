c6c87117ffefa60f61f104b1915ac3c2ebd172c3
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-11-14 10:35:33 -0500

    tercer cambio

--- ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo01.java
+++ ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo01.java
@@ -22,26 +22,29 @@ public class Ejemplo01 {
         entrada.useLocale(Locale.US);
         System.out.println("Ingrese el nombre del día de la semana");
         String cadena = entrada.nextLine();
+        cadena = cadena.toUpperCase();
         
         switch(cadena){
             case "Lunes":
-                System.out.printf("%s", cadena);
+            case "LUNES":
+            case "lunes":    
+                System.out.printf("%s", cadena.toUpperCase());
                 break;
             
             case "Martes":
             case "martes":
-                System.out.printf("%s", cadena);
+                System.out.printf("%s", cadena.toUpperCase());
                 break;
             
             case "Viernes":
             case "VIERNES":
             case "viernes":
-                System.out.printf("%s", cadena);
+                System.out.printf("%s", cadena.toUpperCase());
                 break;
             
             case "Domingo":
                 System.out.printf("%s, es el séptimo día "
-                        + "de la semana", cadena);
+                        + "de la semana", cadena.toUpperCase());
                 break;
                 
             default:
